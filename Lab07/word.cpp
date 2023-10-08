d6868612d3db6bcbcda0318965481673cf178b31
Tusty <140313945+tusty26@users.noreply.github.com>
2023-10-08 15:12:11 +0600

    Add files via upload

--- Lab07/word.cpp
+++ Lab07/word.cpp
@@ -1,1 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int low=0,up=0;
+    string s;
+    cin >> s;
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i]>=97)
+            low++;
+        else
+            up++;
+    }
+    if(low>=up)
+    {
+        //for ( range_declaration : range_expression )
+
+        for(auto x:s)
+        {
+
+            cout << (char)tolower(x);
+        }
+
+    }
+    else
+    {
+        for(auto x:s)
+        {
+
+            cout << (char)toupper(x);
+        }
+    }
+
+
+    return 0;
+}
